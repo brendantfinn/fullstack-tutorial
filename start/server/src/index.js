c593454d56f6e76febd3c09ddcf1cb77dc0a3291
brendantfinn <brendantfinn@gmail.com>
2020-07-09 16:03:59 -0500

    connected to data sources

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,21 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+// createStore function to set up our SQLite database. 
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
 
+const store = createStore();
+
+const server = new ApolloServer({ 
+  typeDefs,
+  dataSources: () => ({
+    // dataSources function to the ApolloServer constructor to connect instances of LaunchAPI and UserAPI to our graph. 
+    LaunchAPI: new LaunchAPI(),
+    UserAPI: new UserAPI({ store })
+  }) 
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
